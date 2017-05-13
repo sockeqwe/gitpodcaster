443b40178ca9e0cde02ef375ceb92f8166d236ae
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 01:56:11 +0200

    Automation test

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,7 @@ BRANCH="master"
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_BRANCH" == "$BRANCH" ];
+elif [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
   echo "Skipping snapshot deployment: was not a pull request."
 else
   ./gradlew build podcast
