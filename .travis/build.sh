d1ad1cf7ed93ce8ff8fe28ba4ce680dc63b4eb1b
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 08:32:07 +0200

    Automation test

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,8 +4,6 @@ BRANCH="master"
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping podcast generation. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_PULL_REQUEST" != "true" ]; then
-  echo "Skipping podcast generation: was not a pull request."
 elif [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   echo "Skipping podcast generation: directly push to master branch"
 else
