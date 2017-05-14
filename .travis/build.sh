f2f865f0f481b447869b541347cf9ff816cec125
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 22:45:08 +0200

    Retry, more logs

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,12 +3,14 @@ SLUG="sockeqwe/gitpodcaster"
 BRANCH="master"
 COMMIT_MSG="Auto Genreated podcast stuff"
 
+git status
+
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping podcast generation. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
 #elif [ "$TRAVIS_PULL_REQUEST" != "true" ]; then
 #  echo "Skipping podcast generation: was not a pull request."
 elif [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
-  echo "Skipping podcast generation: directly push to master branch: $TRAVIS_BRANCH"
+  echo "Skipping podcast generation: directly push to master branch. Branch is $TRAVIS_BRANCH"
 elif [ "$TRAVIS_COMMIT_MESSAGE" = "$COMMIT_MSG" ]; then
   echo "Skipping podcast generation: push was caused by auto generated push"
 else
