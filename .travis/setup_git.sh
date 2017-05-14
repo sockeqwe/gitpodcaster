b0f03378b6a0dc029c62c110e6ee5addc3b412de
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 02:07:27 +0200

    Automation test

--- .travis/setup_git.sh
+++ .travis/setup_git.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 git remote set-url origin https://sockeqwe:${GH_TOKEN}@github.com/sockeqwe/gitpodcaster.git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
