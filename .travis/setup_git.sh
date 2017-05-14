9bbc10c09ea273a641c578164f267e504355aaec
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 10:15:52 +0200

    Preventing travis ci infinite loops

--- .travis/setup_git.sh
+++ .travis/setup_git.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+chmod 777 /home/travis/build/sockeqwe/gitpodcaster/
 git remote set-url origin https://sockeqwe:${GH_TOKEN}@github.com/sockeqwe/gitpodcaster.git
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
