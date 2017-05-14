ebbdceebf60046af2f830fd91dcb24920ad6ed42
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 12:58:30 +0200

    Adding Episode 3

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,8 +5,8 @@ COMMIT_MSG="Auto Genreated podcast stuff"
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping podcast generation. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo "Skipping podcast generation: was not a pull request."
+#elif [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+#  echo "Skipping podcast generation: was not a pull request."
 elif [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   echo "Skipping podcast generation: directly push to master branch"
 elif [ "$TRAVIS_COMMIT_MESSAGE" = "$COMMIT_MSG" ]; then
