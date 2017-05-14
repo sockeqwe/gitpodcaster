65cf48331231eae91f3f67ff1c84e470769d3df0
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 02:01:27 +0200

    Automation test

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-git add .
-git commit -am "Genreated podcast stuff"
-git push
+SLUG="sockeqwe/gitpodcaster"
+BRANCH="master"
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: was not a pull request."
+else
+  git add .
+  git commit -am "Genreated podcast stuff"
+  git push
+fi
