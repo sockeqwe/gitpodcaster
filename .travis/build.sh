20c81010099be7412325437381be9380c12f3418
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 10:13:12 +0200

    Preventing travis ci infinite loops

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 SLUG="sockeqwe/gitpodcaster"
 BRANCH="master"
+COMMIT_MSG="Auto Genreated podcast stuff"
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping podcast generation. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+# elif [ "$TRAVIS_PULL_REQUEST" != "true" ]; then
+#  echo "Skipping podcast generation: was not a pull request."
 elif [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   echo "Skipping podcast generation: directly push to master branch"
+elif [ "$TRAVIS_COMMIT_MESSAGE" = "$COMMIT_MSG" ]; then
+  echo "Skipping podcast generation: push was caused by auto generated push"
 else
   ./gradlew build podcast
 fi
--- .travis/push.sh
+++ .travis/push.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SLUG="sockeqwe/gitpodcaster"
 BRANCH="master"
+COMMIT_MSG="Auto Genreated podcast stuff"
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping podcast generation. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
@@ -8,11 +9,13 @@ if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
 #  echo "Skipping podcast generation: was not a pull request."
 elif [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   echo "Skipping podcast generation: directly push to master branch"
+elif [ "$TRAVIS_COMMIT_MESSAGE" = "$COMMIT_MSG" ]; then
+  echo "Skipping podcast generation: push was caused by auto generated push"
 else
   git status
   git checkout $TRAVIS_BRANCH
   git add .
-  git commit -am "Genreated podcast stuff"
+  git commit -am $COMMIT_MSG
   git push origin $TRAVIS_BRANCH
   git status
 fi
