5f292a6e8973057050a2257cc636ed5dac05c01f
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-05-14 08:30:01 +0200

    Automation test

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,9 +3,11 @@ SLUG="sockeqwe/gitpodcaster"
 BRANCH="master"
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
-  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
-  echo "Skipping snapshot deployment: was not a pull request."
+  echo "Skipping podcast generation. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_PULL_REQUEST" != "true" ]; then
+  echo "Skipping podcast generation: was not a pull request."
+elif [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
+  echo "Skipping podcast generation: directly push to master branch"
 else
   ./gradlew build podcast
 fi
